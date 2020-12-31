593ac4350b878edd846f70e8b4bf858911cf003e
Marek Bartík <bartimar6@gmail.com>
2020-12-31 11:22:35 +0100

    chore: remove commented provider

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# provider "google" {
-#   project = var.project
-#   region  = var.region
-# }
-
 locals {
   instance_name = format("%s-%s", var.instance_name, substr(md5(module.gce-container.container.image), 0, 8))
   target_tags   = ["ci", "atlantis"]
